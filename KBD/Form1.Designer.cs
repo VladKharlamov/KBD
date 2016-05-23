b6916e3de8b307421b2850465af0bc0ddac629e2
Vlad <Vlad@DESKTOP-1M3L0V4>
2016-05-23 10:41:35 +0300

    Добавьте файлы проекта.

---
+++ KBD/Form1.Designer.cs
@@ -0,0 +1,361 @@
+﻿namespace KBD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.userNameTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SelectButton = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.AddTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.DeleteTextBox = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.CountButton = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.AllowDrop = true;
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1185, 567);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.passwordTextBox);
+            this.tabPage1.Controls.Add(this.userNameTextBox);
+            this.tabPage1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tabPage1.Location = new System.Drawing.Point(4, 27);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1177, 536);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Подключение к БД";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.CountButton);
+            this.tabPage2.Controls.Add(this.button3);
+            this.tabPage2.Controls.Add(this.DeleteTextBox);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.button2);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.AddTextBox);
+            this.tabPage2.Controls.Add(this.groupBox1);
+            this.tabPage2.Controls.Add(this.SelectButton);
+            this.tabPage2.Controls.Add(this.comboBox1);
+            this.tabPage2.Controls.Add(this.dataGridView1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 27);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1177, 536);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Работа с БД";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // userNameTextBox
+            // 
+            this.userNameTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.userNameTextBox.Location = new System.Drawing.Point(474, 221);
+            this.userNameTextBox.Name = "userNameTextBox";
+            this.userNameTextBox.Size = new System.Drawing.Size(286, 24);
+            this.userNameTextBox.TabIndex = 0;
+            this.userNameTextBox.Text = "Введите логин";
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.passwordTextBox.Location = new System.Drawing.Point(474, 268);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(286, 24);
+            this.passwordTextBox.TabIndex = 1;
+            this.passwordTextBox.Text = "Введите пароль";
+            this.passwordTextBox.UseSystemPasswordChar = true;
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(474, 317);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(286, 36);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(403, 161);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(401, 28);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Выполните подключение к базе данных";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(8, 6);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(805, 524);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Судна",
+            "Рейсы",
+            "Накладные",
+            "Контейнеры",
+            "Накладные-Контейнеры"});
+            this.comboBox1.Location = new System.Drawing.Point(832, 33);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(142, 26);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.Text = "Судна";
+            // 
+            // SelectButton
+            // 
+            this.SelectButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.SelectButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.SelectButton.Location = new System.Drawing.Point(992, 24);
+            this.SelectButton.Name = "SelectButton";
+            this.SelectButton.Size = new System.Drawing.Size(161, 42);
+            this.SelectButton.TabIndex = 2;
+            this.SelectButton.Text = "Вывести таблицу";
+            this.SelectButton.UseVisualStyleBackColor = true;
+            this.SelectButton.Click += new System.EventHandler(this.SelectButton_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton4);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(832, 134);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(321, 113);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Выберите таблицу для редактирования";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 36);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(72, 22);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Судна";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(6, 74);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(74, 22);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.Text = "Рейсы";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(190, 36);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(109, 22);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.Text = "Накладные";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(190, 74);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(114, 22);
+            this.radioButton4.TabIndex = 3;
+            this.radioButton4.Text = "Контейнеры";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // AddTextBox
+            // 
+            this.AddTextBox.Location = new System.Drawing.Point(832, 304);
+            this.AddTextBox.Name = "AddTextBox";
+            this.AddTextBox.Size = new System.Drawing.Size(321, 24);
+            this.AddTextBox.TabIndex = 4;
+            this.AddTextBox.Text = "\'Argo2\', \'Tanker\', \'900000\'";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(832, 268);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(200, 18);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Добавить строку в таблицу";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(832, 353);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(321, 32);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Добавить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(832, 411);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(202, 18);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Удалить строку из таблицы";
+            // 
+            // DeleteTextBox
+            // 
+            this.DeleteTextBox.Location = new System.Drawing.Point(832, 441);
+            this.DeleteTextBox.Name = "DeleteTextBox";
+            this.DeleteTextBox.Size = new System.Drawing.Size(321, 24);
+            this.DeleteTextBox.TabIndex = 8;
+            this.DeleteTextBox.Text = "\'Argo2\'";
+            // 
+            // button3
+            // 
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(832, 486);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(321, 32);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Удалить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // CountButton
+            // 
+            this.CountButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CountButton.Location = new System.Drawing.Point(832, 84);
+            this.CountButton.Name = "CountButton";
+            this.CountButton.Size = new System.Drawing.Size(321, 32);
+            this.CountButton.TabIndex = 10;
+            this.CountButton.Text = "Количество ";
+            this.CountButton.UseVisualStyleBackColor = true;
+            this.CountButton.Click += new System.EventHandler(this.CountButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1185, 567);
+            this.Controls.Add(this.tabControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "Робота морського порту";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.TextBox userNameTextBox;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button SelectButton;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox AddTextBox;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox DeleteTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button CountButton;
+    }
+}
+
---
+++ KBD/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace KBD
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        SqlConnectionStringBuilder connectionStringBuilder = new SqlConnectionStringBuilder();
+        SqlConnection connection;
+        private void Select(SqlConnection connection, string s, string s1)
+        {
+            SqlDataAdapter da = new SqlDataAdapter(s, connection);
+
+            SqlCommandBuilder cb = new SqlCommandBuilder(da);
+
+            DataSet ds = new DataSet();
+
+            da.Fill(ds, s1);
+            dataGridView1.DataSource = ds.Tables[0];
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            connectionStringBuilder.DataSource = @".\SQLEXPRESS";       
+            connectionStringBuilder.InitialCatalog = "SEAPORT";          
+            connectionStringBuilder.UserID = userNameTextBox.Text;      
+            connectionStringBuilder.Password = passwordTextBox.Text;
+
+            connection = new SqlConnection(connectionStringBuilder.ConnectionString);
+
+            try
+            {
+                    connection.Open();
+                    MessageBox.Show("Подключение выполено");
+                    tabControl1.SelectedIndex = 1;
+                    Select(connection, "SELECT * FROM Ship", "Ship");
+                }
+                catch (Exception exception)
+                {
+                    MessageBox.Show(exception.Message);
+                }
+                finally
+                {
+                    //connection.Close();
+                }
+                
+        }
+
+        private void SelectButton_Click(object sender, EventArgs e)
+        {
+            switch (comboBox1.SelectedItem.ToString())
+            {
+                case "Судна": Select(connection, "SELECT * FROM Ship", "Ship"); break;
+                case "Рейсы": Select(connection, "SELECT * FROM Voyage", "Ship"); break;
+                case "Накладные": Select(connection, "SELECT * FROM Invoice", "Ship"); break;
+                case "Контейнеры": Select(connection, "SELECT * FROM Container", "Ship"); break;
+                case "Накладные-Контейнеры": Select(connection, "SELECT * FROM InvoiceContainer", "Ship"); break;
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string temp="";
+            if (radioButton1.Checked) temp = "Ship";
+            if (radioButton2.Checked) temp = "Voyage";
+            if (radioButton3.Checked) temp = "Invoice";
+            if (radioButton4.Checked) temp = "Container";
+
+            string s = "INSERT INTO " + temp + " VALUES(" + AddTextBox.Text + ")";
+            SqlCommand insertCommand = connection.CreateCommand(); 
+            insertCommand.CommandText = s;
+
+            int rowAffected = insertCommand.ExecuteNonQuery();
+            MessageBox.Show("Строка добавлена");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //if (connection != null)
+           // {
+                connection.Close();
+                MessageBox.Show("Подключение закрыто");
+           // }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string s = "";
+            if (radioButton1.Checked)
+                s = "DELETE Ship WHERE sname = " + DeleteTextBox.Text;
+
+            if (radioButton2.Checked)
+                s = "DELETE Voyage WHERE vnumber = " + DeleteTextBox.Text;
+
+            if (radioButton3.Checked)
+                s = "DELETE Invoice WHERE invnumber = " + DeleteTextBox.Text;
+
+            if (radioButton4.Checked)     
+                s = "DELETE Container WHERE cnumber = " + DeleteTextBox.Text;
+            
+
+            SqlCommand insertCommand = connection.CreateCommand();
+            insertCommand.CommandText = s;
+
+            int rowAffected = insertCommand.ExecuteNonQuery();
+            MessageBox.Show("Строка удалена");
+        }
+
+        private void CountButton_Click(object sender, EventArgs e)
+        {
+            switch (comboBox1.SelectedItem.ToString())
+            {
+                case "Судна":
+                    {
+                        SqlCommand countCommand = connection.CreateCommand();
+                        countCommand.CommandText = "SELECT COUNT(*) FROM Ship";
+                        MessageBox.Show("Количество: "+ Convert.ToString(countCommand.ExecuteScalar()));
+                        break;
+                    }
+                case "Рейсы":
+                    {
+                        SqlCommand countCommand = connection.CreateCommand();
+                        countCommand.CommandText = "SELECT COUNT(*) FROM Voyage";
+                        MessageBox.Show("Количество: " + Convert.ToString(countCommand.ExecuteScalar()));
+                        break;
+                    }
+                case "Накладные":
+                    {
+                        SqlCommand countCommand = connection.CreateCommand();
+                        countCommand.CommandText = "SELECT COUNT(*) FROM Invoice";
+                        MessageBox.Show("Количество: " + Convert.ToString(countCommand.ExecuteScalar()));
+                        break;
+                    }
+                case "Контейнеры":
+                    {
+                        SqlCommand countCommand = connection.CreateCommand();
+                        countCommand.CommandText = "SELECT COUNT(*) FROM Container";
+                        MessageBox.Show("Количество: " + Convert.ToString(countCommand.ExecuteScalar()));
+                        break;
+                    }
+                case "Накладные-Контейнеры":
+                    {
+                        SqlCommand countCommand = connection.CreateCommand();
+                        countCommand.CommandText = "SELECT COUNT(*) FROM InvoiceContainer";
+                        MessageBox.Show("Количество: " + Convert.ToString(countCommand.ExecuteScalar()));
+                        break;
+                    }
+            }
+        }
+    }
+}
